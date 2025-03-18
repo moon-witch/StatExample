4415e8ccf54128cef3175e7df37288debae1f4f3
moonwitch <joshua_develops@proton.me>
2025-03-18 17:44:45 +0100

    feat: added basic navbar and unified sass styling system

---
+++ src/components/navbar/NavItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <li>
+    <RouterLink to="/about">{{ title }}</RouterLink>
+  </li>
+</template>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import NavItem from './NavItem.vue';
+</script>
+
+<template>
+  <nav class="nav">
+    <img class="logo" type="logo" src="/favicon/favicon.svg" alt="logo"/>
+    <ul class="list">
+      <NavItem title="Home"/>
+      <NavItem title="About"/>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.nav {
+
+}
+</style>
